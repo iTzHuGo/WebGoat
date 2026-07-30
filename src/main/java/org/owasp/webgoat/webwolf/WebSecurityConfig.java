1a43d4a7e85363be3cc79c84c67e2540658ac561
Hugo Barros <hugosbarros@gmail.com>
2026-07-30 22:37:35 +0100

    CWE-327-Injected: injected mutation

--- src/main/java/org/owasp/webgoat/webwolf/WebSecurityConfig.java
+++ src/main/java/org/owasp/webgoat/webwolf/WebSecurityConfig.java
@@ -17,7 +17,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.MessageDigestPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 /** Security configuration for WebWolf. */
@@ -82,7 +82,7 @@ public class WebSecurityConfig {
   }
 
   @Bean
-  public NoOpPasswordEncoder passwordEncoder() {
-    return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+  public MessageDigestPasswordEncoder passwordEncoder() {
+    return new MessageDigestPasswordEncoder("MD5");
   }
 }
